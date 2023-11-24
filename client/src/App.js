70b9caebae044d54b9f11835c899414381de1ff1
unknown <rocio.sosa94@gmail.com>
2023-11-24 16:54:19 -0300

    rendering front

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
-
-function App() {
+//import './App.css';
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import LandingPage from './components/landingPage/LandingPage'
+const App= ()=> {
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
+      <h1>Henry Dogs</h1> 
+      <BrowserRouter>
+      <Switch>
+        <Route  exact path="/" component={LandingPage}/>
+        <Route></Route>
+        <Route></Route>
+        <Route></Route>
+      </Switch>
+      </BrowserRouter>
+    
     </div>
   );
 }
--- client/src/components/landingPage/LandingPage.jsx
+++ client/src/components/landingPage/LandingPage.jsx
@@ -1,16 +1,14 @@
+import {Link} from "react-router-dom";
+
 const LandingPage = ()=>{
     return (
     <div>
         <h1>My Dogs App</h1>
         <div>
-            <img>
-            
-            </img>
+            <img/>
         </div>
-        <Link>
-        <button>
-            Home
-        </button>
+        <Link to="/">
+        <button>Home</button>
         </Link>
     </div>
     )
