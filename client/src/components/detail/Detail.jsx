5e0f98af58b5ce690a1456f338b39418ad55a2a0
unknown <rocio.sosa94@gmail.com>
2023-12-04 11:09:04 -0300

    detail

--- client/src/components/detail/Detail.jsx
+++ client/src/components/detail/Detail.jsx
@@ -1,37 +1,35 @@
 // Hooks
 import { useEffect, useState } from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 //Dependencies
 import axios from "axios";
-
-
+//style
+import style from "./Detail.module.css";
 
 const Detail = () => {
+  const { idRaza } = useParams();
+  const [dogDetail, setDogDetail] = useState({});
 
-    const {idRaza} = useParams();
-    const [dogDetail, setDogDetail] = useState({});
-
-    useEffect(()=> {
-        const dog= async ()=>{
-            const {data}= await axios.get (`http://localhost:3001/dogs/${idRaza}`)
-            setDogDetail(data)
-        }
-        dog()
-         return (()=>{
-            setDogDetail({})
-         })
-    }, [idRaza])
-    return (
-        <div className={style.detailContainer}>
-            <h3>Name:{dogDetail.name}</h3>
-            <h4>Id:{dogDetail.id}</h4>
-            <h5>Weight:{dogDetail.weight}</h5>
-            <h5>Height:{dogDetail.height}</h5>
-            <h5>Life span:{dogDetail.life_span}</h5>
-            <h5>Temperament:{dogDetail.temperament}</h5>
-            <img src={dogDetail.image} alt={dogDetail.name}/>
-
-        </div>
-    )
+  useEffect(() => {
+    const dog = async () => {
+      const { data } = await axios.get(`http://localhost:3001/dogs/${idRaza}`);
+      setDogDetail(data);
+    };
+    dog();
+    return () => {
+      setDogDetail({});
+    };
+  }, [idRaza]);
+  return (
+    <div className={style.detailContainer}>
+      <h2 className={style.detailName}>Name:{dogDetail.name}</h2>
+      <p className={style.detailP}>Id:{dogDetail.id}</p>
+      <p className={style.detailP}>Weight:{dogDetail.weight}</p>
+      <p className={style.detailP}>Height:{dogDetail.height}</p>
+      <p className={style.detailP}>Life span:{dogDetail.life_span}</p>
+      <p className={style.detailP}>Temperament:{dogDetail.temperament}</p>
+      <img src={dogDetail.image} alt={dogDetail.name} className={style.imageDetail} />
+    </div>
+  );
 };
 export default Detail;
